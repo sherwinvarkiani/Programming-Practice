4b3438037a9d9702a4db6206f62875ffe06ef0b7
sherwinvarkiani <s.varkiani@gmail.com>
2020-02-16 23:54:04 -0500

    Working on it

--- OpenCV/ContourObjectDetection.py
+++ OpenCV/ContourObjectDetection.py
@@ -1,7 +1,7 @@
 import numpy as np 
 import cv2
 
-img = cv2.imread('detect_blob.png', 1)
+img = cv2.imread('./detect_blob.png', 1)
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 1)
 cv2.imshow("Binary", thresh)
